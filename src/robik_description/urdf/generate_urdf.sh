a1aedf1a66ac7ab78636678f7b1038092950a2f8
Jan Slesinger (DHL IT Services) <jan.slesinger@dhl.com>
2020-01-31 22:41:03 +0100

    urdf

--- src/robik_description/urdf/generate_urdf.sh
+++ src/robik_description/urdf/generate_urdf.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 rosrun xacro xacro `rospack find robik_description`/urdf/robik.urdf.xacro -o ./robik.urdf
+
+echo 'Must be installed: sudo apt-get install liburdfdom-tools'
+check_urdf ./robik.urdf
+urdf_to_graphiz ./robik.urdf
+rm ./robik.gv
+evince ./robik.pdf
